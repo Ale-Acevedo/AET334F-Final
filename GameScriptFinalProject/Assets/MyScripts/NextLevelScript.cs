02c7b216d02dc975fa881800efb4ddaf19a47daf
Sharksong <63263082+Sharksong@users.noreply.github.com>
2021-04-26 12:35:08 -0500

    Updated Follower Ghast enemy sprite

--- GameScriptFinalProject/Assets/MyScripts/NextLevelScript.cs
+++ GameScriptFinalProject/Assets/MyScripts/NextLevelScript.cs
@@ -7,13 +7,11 @@ public class NextLevelScript : MonoBehaviour
 {
     //Written by Kasey. 
     
-    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
 
