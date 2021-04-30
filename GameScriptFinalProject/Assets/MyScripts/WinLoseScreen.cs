72b5b3b60439f0e0b01dbd4f5fc100a5ac28e1fa
Sharksong <63263082+Sharksong@users.noreply.github.com>
2021-04-29 22:50:38 -0500

    Made "Restart" button on Game Over screen go back to level 1

--- GameScriptFinalProject/Assets/MyScripts/WinLoseScreen.cs
+++ GameScriptFinalProject/Assets/MyScripts/WinLoseScreen.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class WinScreen : MonoBehaviour
+public class WinLoseScreen : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+    //Returns the player to the main menu. 
     public void PlayAgain()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    //Returns the player to the first level.
+    public void Restart()
+    {
+        SceneManager.LoadScene("Puzzle 1");
     }
 }
