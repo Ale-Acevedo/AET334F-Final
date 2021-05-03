f0121f1dc4b5d28ec42e5752bddc75fb1ae2e2d2
Jay <jacobalvarado36@gmail.com>
2021-05-03 09:55:22 -0500

    Slight level 3 adjustment + Code Commentary

--- GameScriptFinalProject/Assets/MyScripts/Puzzle Scripts/Box.cs
+++ GameScriptFinalProject/Assets/MyScripts/Puzzle Scripts/Box.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Written by Jacob
+//The graves were boxes at one point, and I didn't want to risk changing an important variable as too many of them rely on "Box", so box = grave
+
 public class Box : MonoBehaviour
 {
-    public GameObject nextButton;
+    public GameObject nextButton; //States the Next Button variable 
     public bool OnCross; //True if box has been pushed on to a cross
 
     public bool Move(Vector2 direction) //Avoid abiliy to move diagonolally
     {
-        if (BoxBlocked(transform.position, direction))
+        if (BoxBlocked(transform.position, direction)) 
         {
-            return false;
+            return false; //Box is blocked and cannot be moved
         }
         else
         {
@@ -24,38 +27,38 @@ public class Box : MonoBehaviour
     bool BoxBlocked(Vector3 position, Vector2 direction) // Boxes blocked by other boxes and walls
     {
         Vector2 newPos = new Vector2(position.x, position.y) + direction;
-        GameObject[] walls = GameObject.FindGameObjectsWithTag("Wall");
+        GameObject[] walls = GameObject.FindGameObjectsWithTag("Wall"); //finds objects with the wall tag
 
         foreach (var wall in walls)
         {
             if (wall.transform.position.x == newPos.x && wall.transform.position.y == newPos.y)
             {
-                return true;
+                return true; //box is blocked
             }
         }
-        GameObject[] boxes = GameObject.FindGameObjectsWithTag("Box");
+        GameObject[] boxes = GameObject.FindGameObjectsWithTag("Box"); //finds objects with the wall tag
 
         foreach (var box in boxes)
         {
             if (box.transform.position.x == newPos.x && box.transform.position.y == newPos.y)
             { 
-                return true;
+                return true; //box is blocked
             }
         }
 
-        return false;
+        return false; //do not block box
     }
 
-    void TestForOnCross()
+    void TestForOnCross() //tests if the graves are on the crosses
     {
         GameObject[] crosses = GameObject.FindGameObjectsWithTag("Cross");
         foreach (var cross in crosses)
         {
-            if (transform.position.x == cross.transform.position.x && transform.position.y == cross.transform.position.y)
+            if (transform.position.x == cross.transform.position.x && transform.position.y == cross.transform.position.y) //if the grave position is the same as a cross position
             {   //On a cross
-                GetComponent<SpriteRenderer>().color = Color.red;
-                OnCross = true;
-                GameObject.Find("SFXManager").GetComponent<AudioSource>().PlayOneShot(GameObject.Find("SFXManager").GetComponent<SFXManager>().sounds[2]);
+                GetComponent<SpriteRenderer>().color = Color.red; //turns the graves red whenever they are moved on a cross
+                OnCross = true; //sets OnCross boolean to true
+                GameObject.Find("SFXManager").GetComponent<AudioSource>().PlayOneShot(GameObject.Find("SFXManager").GetComponent<SFXManager>().sounds[2]); //plays audio
                 return;
             }          
         }      
--- GameScriptFinalProject/Assets/MyScripts/Puzzle Scripts/GameManager.cs
+++ GameScriptFinalProject/Assets/MyScripts/Puzzle Scripts/GameManager.cs
@@ -3,26 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//Written by Jacob
+//Note: Anything with SFX manager and fxToggle was written by Ale, so I'm not 100% sure how it works, I'll do my best to explain it though
+
 public class GameManager : MonoBehaviour
 {
-    public LevelBuilder  m_LevelBuilder;
-    public GameObject m_NextButton;
-    private bool m_ReadyForInput;
-    public Player m_Player;
-    private bool fxToggle = true;
+    public LevelBuilder  m_LevelBuilder; //Not Applicable
+    public GameObject m_NextButton; //Next Level Button that will appear after certain requirements are met
+    private bool m_ReadyForInput; //Boolean 
+    public Player m_Player; //Player variable
+    private bool fxToggle = true; //Boolean that sets FX toggle on when true, or when level is not complete 
 
     public bool puzzleSolved; //Public bool to let NextLevelScript know when the player has completed the puzzle. - Kasey
 
     void Start()
     {
-        m_NextButton.SetActive(false);
+        m_NextButton.SetActive(false); //ensures that the Next Level button will not activate when the level begins 
 
     }
     void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        moveInput.Normalize();
-        if (moveInput.sqrMagnitude > .5) //Button pressed or held
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); //establishes movement
+        moveInput.Normalize(); //sets movement input to exact values (1)
+        if (moveInput.sqrMagnitude > .5) //If button pressed or held, to this certain magnitude or above, move an exact value in any direction
         {
             if (m_ReadyForInput)
             {
@@ -40,20 +43,20 @@ public class GameManager : MonoBehaviour
 
         if (puzzleSolved && fxToggle)
         {
-            GameObject.Find("SFXManager").GetComponent<AudioSource>().PlayOneShot(GameObject.Find("SFXManager").GetComponent<SFXManager>().sounds[4]);
-            fxToggle = false;
+            GameObject.Find("SFXManager").GetComponent<AudioSource>().PlayOneShot(GameObject.Find("SFXManager").GetComponent<SFXManager>().sounds[4]); //if puzzle is solved, play different music/sound effectts
+            fxToggle = false; //fxToggle is set to false when level is complete
         }
     }
 
     
     public void NextLevel()
     {
-        m_NextButton.SetActive(false);
+        m_NextButton.SetActive(false); //sets Next Level button to false
     }
 
     public void ResetScene()
     {
-        SceneManager.LoadScene("Puzzle 1");
+        SceneManager.LoadScene("Puzzle 1"); //Loads Puzzle 1 again, or "resets" the scene
     }
 
     bool IsLevelComplete()
@@ -61,12 +64,12 @@ public class GameManager : MonoBehaviour
         Box[] boxes = FindObjectsOfType<Box>();
         foreach (var box in boxes)
         {
-            if (!box.OnCross) return false;
+            if (!box.OnCross) return false; //if boxes are not on crosses, level is not complete, and Next Level button does not show
         }
 
         
         return true;
-        m_NextButton.SetActive(true);
+        m_NextButton.SetActive(true); //boxes are on crosses, so the level is complete; Next Level button appears (this was replaced with the yellow heart by Ale and Kasey somehow)
     }
 
     
